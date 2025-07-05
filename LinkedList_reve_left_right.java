e2ef7c652be43a8e39e1b98223e2b5305b2d861c
Shovan Paria <shovanparia7@gmail.com>
2025-07-05 20:20:34 +0530

    Add files via upload

---
+++ LinkedList_reve_left_right.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+
+public class LinkedList_reve_left_right {
+    public static void main(String[] args) {
+        LinkedList<Integer> list=new LinkedList<>();
+        list.add(0, list.get(1));
+       ;
+    }
+}
