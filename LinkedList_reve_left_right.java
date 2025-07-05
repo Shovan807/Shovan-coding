01f21e81037ec480e23b5039ac3ac1c4903b347f
Shovan Paria <shovanparia7@gmail.com>
2025-07-05 20:25:30 +0530

    Delete LinkedList_reve_left_right.java

--- LinkedList_reve_left_right.java
+++
@@ -1,9 +0,0 @@
-import java.util.LinkedList;
-
-public class LinkedList_reve_left_right {
-    public static void main(String[] args) {
-        LinkedList<Integer> list=new LinkedList<>();
-        list.add(0, list.get(1));
-       ;
-    }
-}
